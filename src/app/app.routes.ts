e1cb20403ea48a1d78a63ad6d0f4b3cfe9de93cc
Kamil Szewczyk <szewdev@hotmail.com>
2023-12-28 17:07:37 +0100

    feat: Product Routing (initial commit)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {ProductListComponent} from "./components/product-list/product-list.component";
+import {ProductFormComponent} from "./components/product-form/product-form.component";
+import {ProductDetailsComponent} from "./components/product-details/product-details.component";
+import {HomeComponent} from "./components/home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'products', component: ProductListComponent},
+  {path: 'add-product', component: ProductFormComponent},
+  {path: 'edit-product/:id', component: ProductFormComponent},
+  {path: 'product-details/:id', component: ProductDetailsComponent},
+  {path: '', component: HomeComponent, pathMatch: 'full'}
+];
