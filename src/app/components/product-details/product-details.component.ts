6e653d9d66099abd12b4b2b60c69d87a3f00b028
Kamil Szewczyk <szewdev@hotmail.com>
2023-12-29 15:28:31 +0100

    feat: Product details

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router, RouterLink} from "@angular/router";
+import {Product} from "../../models/product.model";
+import {ProductService} from "../../services/product.service";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    NgIf,
+    RouterLink
+  ],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+  productId: number | null = null;
+  product: Product | undefined = undefined;
 
+  constructor(private route: ActivatedRoute, private router: Router, private productService: ProductService) {
+
+  }
+
+  ngOnInit(): void {
+    this.productId = Number(this.route.snapshot.paramMap.get('id'));
+    if (!this.productId) {
+      this.router.navigate(['/products']);
+      return;
+    }
+
+    this.productService.getProduct(this.productId).subscribe({
+      next: (product: Product) => {
+        console.log(product);
+        this.product = product;
+      },
+      error: (e) => {
+        console.error(e);
+      },
+      complete() {
+        console.log("is completed");
+      },
+    });
+  }
 }
+
+
