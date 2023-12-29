74345b872b9ccc77ba431087a7d6ea88f30b5d71
Kamil Szewczyk <szewdev@hotmail.com>
2023-12-29 15:29:04 +0100

    feat: Create product (form)

--- src/app/components/product-form/product-form.component.ts
+++ src/app/components/product-form/product-form.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {ProductService} from "../../services/product.service";
+import {NgIf} from "@angular/common";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-product-form',
-  standalone: true,
-  imports: [],
   templateUrl: './product-form.component.html',
-  styleUrl: './product-form.component.css'
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    NgIf
+  ],
+  styleUrls: ['./product-form.component.css']
 })
-export class ProductFormComponent {
+export class ProductFormComponent implements OnInit {
+  productForm: FormGroup;
+  submitted = false;
+  success = false;
 
+  constructor(private route: ActivatedRoute, private router: Router, private formBuilder: FormBuilder, private productService: ProductService) {
+    this.productForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      price: ['', [Validators.required, Validators.min(0.01)]],
+      description: []
+    });
+  }
+
+  ngOnInit(): void {
+    //todo ksz edit product
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.productForm.invalid) {
+      return;
+    }
+    this.productService.addProduct(this.productForm.value).subscribe({
+      next: (data: any) => {
+        console.log(data);
+        this.success = true;
+        setTimeout(() => {
+            this.router.navigate(['/products']);
+          },
+          1500);
+      },
+      error: (error: any) => {
+        console.error('Error adding product!', error);
+        this.success = false;
+      }
+    });
+  }
 }
